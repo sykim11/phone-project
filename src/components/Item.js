42e3126326dd4da4fa86a0a1f399ac5c653109d4
MSDN-SPECIAL\Administrator <ymt45@naver.com>
2017-06-18 17:52:16 +0900

    state/setstate/react-addons-update 예제

--- src/components/Item.js
+++ src/components/Item.js
@@ -4,15 +4,33 @@ import './Item.css';
 class Item extends React.Component{
 
   render(){
+
+    // let data = [
+    //   {content: '공부하기'},
+    //   {content: '놀기'},
+    //   {content: '밥먹기'},
+    //   {content: '울기'},
+    //   {content: '잠자기'}
+    // ]
+
+    // map 함수
+    // let a = [1,2,3,4,5]
+
     return (
 
           <div className="list-section">
 
-            <div className="box">
-              <div className="title">HTML 배우기</div>
-              <span className="btn edit">수정</span>
-              <span className="btn">삭제</span>
-            </div>
+            {
+              // a.map(function(num){ return num })
+              this.props.itemList.map((item)=>{
+                return (
+                  <div className="box">
+                    <div className="title">{item.content}</div>
+                    <span className="btn edit">수정</span><span className="btn">삭제</span>
+                  </div>
+                )
+              })
+            }
 
           </div>
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import Item from './Item';
 import './List.css';
+// react-addons-update :쉽게 데이터를 넣고 지우고 수정하는 역할의 npm
+import update from 'react-addons-update';
 
 
 class List extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state = {
+      data : []
+    }
+    this.handleKeyUp = this.handleKeyUp.bind(this);
+  }
+
+  handleKeyUp(e){
+
+    if(e.keyCode == 13){
+
+      var inputText = document.querySelector('.contentText').value;
+      // addons의 기능
+      let newState = update(this.state,{
+        data : {
+          $push:[{content:inputText}]
+        }
+      });
+      // addons의 기능 end
+      this.setState(newState);
+      document.querySelector('.contentText').value = '';
+
+    }
+
+  }
 
   render(){
     return (
@@ -11,10 +40,10 @@ class List extends React.Component{
         <div className="inner">
 
             <div className="input-section">
-              <input type="text" name="" placeholder="To Do?" value="" />
+              <input className="contentText" type="text" onKeyUp={this.handleKeyUp} placeholder="To Do?" />
             </div>
 
-          <Item />
+            <Item itemList={this.state.data} />
 
         </div>
 
